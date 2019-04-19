09d7419325c62c94d77c1857624eb06afc39755d
Annia Forney <anniaforney@gmail.com>
2019-04-19 20:46:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,5 +2,10 @@
 badge_maker ("Annia")
 => "Hello, my name is Annia."
 
-badge_creator ("Noah")
-=> "Hello, my name is Noah"
+def batch_badge_creator 
+  puts "Badge creator is Noah."
+end 
+
+def conference badges
+  puts "Hello, ${Zack}! You'll be assigned to room 1"
+end 
